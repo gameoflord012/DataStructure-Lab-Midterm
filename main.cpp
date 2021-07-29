9f1389eda055023740e430cfdf09f3e7f0430908
gameoflord012 <manhhieu012@gmail.com>
2021-07-29 08:05:47 +0700

    data dir

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
-int main()
-{
+#include "json.hpp"
+#include <iostream>
+#include <filesystem>
+#include <string>
 
+
+using namespace std;
+using namespace nlohmann;
+namespace fs = std::filesystem;
+
+int main()
+{   
+    cout << DATA_DIR << endl;
+    string path = DATA_DIR;
+    for (const auto& entry : fs::directory_iterator(path))
+        std::cout << entry.path() << std::endl;
 }
